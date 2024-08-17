cbe0663c04f4abf825275bb92f3df3ab7c9e3990
Chrishart <chrishart.estrada@gmail.com>
2024-08-17 17:21:48 +0800

    First Attempt

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>IshaBlog</title>
+</head>
+<body>
+    <h1>Hello, I'm Isha</h1>
+    <p>I'm a Sissy 💅 who love to express herself on the internet, I'm also a ga(y)mer 🖥.</p>
+    <p>Come join and support me through (insert links)💌, and let's chill together ❤</p>
+    <h3>Subscribe 💌</h3>
+    <form action="" method="get">
+        @csrf
+        <input type="text" name="name" placeholder="Name"><br><br>
+        <input type="email" name="email" placeholder="Email"><br><br>
+        <button type="submit">Subscribe</button>
+    </form>
+</body>
+</html>
