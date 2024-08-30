d230a364e83561f4f03a4f7eafbd0fa47c288959
Chrishart <chrishart.estrada@gmail.com>
2024-08-30 16:22:39 +0800

    LaraReset

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,15 +7,6 @@
     <title>IshaBlog</title>
 </head>
 <body>
-    <h1>Hello, I'm Isha</h1>
-    <p>I'm a Sissy 💅 who love to express herself on the internet, I'm also a ga(y)mer 🖥.</p>
-    <p>Come join and support me through (insert links)💌, and let's chill together ❤</p>
-    <h3>Subscribe 💌</h3>
-    <form action="" method="get">
-        @csrf
-        <input type="text" name="name" placeholder="Name"><br><br>
-        <input type="email" name="email" placeholder="Email"><br><br>
-        <button type="submit">Subscribe</button>
-    </form>
+    <h1>Hello, World!</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('profile');
-
-// Route::redirect('/here', 'there');
+});
